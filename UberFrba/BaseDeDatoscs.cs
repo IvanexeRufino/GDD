a8d8093759073e3dcc674962f59abd98b97d7340
ivanexerufino <ivan-2100@hotmail.com>
2017-04-20 13:06:16 -0300

    Configuracion.txt y conexion a la bd + principio de login

---
+++ UberFrba/BaseDeDatoscs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace UberFrba
+{
+    class BaseDeDatos
+    {
+        public string cadenaconexion;
+        public string schema = "OVERFANTASY";
+        public SqlConnection con;
+
+        public BaseDeDatos() {
+            try {
+                this.cadenaconexion = Configuracion.leerConfiguracion();
+                this.con = new SqlConnection(this.cadenaconexion);
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("No se pudo realizar la conexion", ex.ToString());
+            }
+
+        }
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+    }
+}
