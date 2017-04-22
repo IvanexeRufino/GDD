eb49e057583553791bbd835e9c22c059c8a9e3ae
ivanexerufino <ivan-2100@hotmail.com>
2017-04-22 14:15:53 -0300

    reutilizacion de readers y cmds

--- UberFrba/Menu.cs
+++ UberFrba/Menu.cs
@@ -14,54 +14,62 @@ namespace UberFrba
 {
     public partial class Menu : Form
     {
+        BaseDeDatos bd;
+        SqlConnection conexion;
+
         public Menu()
         {
+            bd = new BaseDeDatos();
+            conexion = bd.getCon();
             InitializeComponent();
         }
 
-        public void desplegarMenu(String nombreDeUsuario, String rol) {
+        public void desplegarMenu(String nombreDeUsuario, String rol)
+        {
 
-            String query = "SELECT Funcionalidad_Descripcion FROM OVERFANTASY.Funcionalidad_Por_Rol WHERE Rol_Nombre = '"+ rol +"'";
-            BaseDeDatos bd = new BaseDeDatos();
-            bd.getCon().Open();
-            SqlCommand cmd = new SqlCommand(query, bd.getCon());
-            SqlDataReader lector = cmd.ExecuteReader();
-
-            while (lector.Read()) {
-                switch (lector.GetString(0))
+            conexion.Open();
+            string query = "SELECT Funcionalidad_Descripcion FROM OVERFANTASY.Funcionalidad_Por_Rol WHERE Rol_Nombre = '" + rol + "'";
+            using (SqlCommand cmd = new SqlCommand(query, conexion))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    case "ABM Rol":
-                        ABMRol.Visible = true;
-                        break;
-                    case "ABM Clientes":
-                        ABMCliente.Visible = true;
-                        break;
-                    case "ABM Chofer":
-                        ABMChofer.Visible = true;
-                        break;
-                    case "ABM Turno":
-                        ABMTurno.Visible = true;
-                        break;
-                    case "ABM Automovil":
-                        ABMAutomovil.Visible = true;
-                        break;
-                    case "Rendicion de Cuentas":
-                        RendicionCuentas.Visible = true;
-                        break;
-                    case "Facturacion a Cliente":
-                        Facturacion.Visible = true;
-                        break;
-                    case "Listado Estadistico":
-                        ListadoEstadistico.Visible = true;
-                        break;
-                    case "Solicitar Viaje":
-                        SolicitarViaje.Visible = true;
-                        break;
+                    while (reader.Read())
+                    {
+                        switch (reader.GetString(0))
+                        {
+                            case "ABM Rol":
+                                ABMRol.Visible = true;
+                                break;
+                            case "ABM Clientes":
+                                ABMCliente.Visible = true;
+                                break;
+                            case "ABM Chofer":
+                                ABMChofer.Visible = true;
+                                break;
+                            case "ABM Turno":
+                                ABMTurno.Visible = true;
+                                break;
+                            case "ABM Automovil":
+                                ABMAutomovil.Visible = true;
+                                break;
+                            case "Rendicion de Cuentas":
+                                RendicionCuentas.Visible = true;
+                                break;
+                            case "Facturacion a Cliente":
+                                Facturacion.Visible = true;
+                                break;
+                            case "Listado Estadistico":
+                                ListadoEstadistico.Visible = true;
+                                break;
+                            case "Solicitar Viaje":
+                                SolicitarViaje.Visible = true;
+                                break;
+                        }
+                    }
                 }
-                bd.getCon().Close();
             }
-
-        }
+            conexion.Close();
+            }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
