864660385379216b82d083b6355400b3425d689b
ivanexerufino <ivan-2100@hotmail.com>
2017-05-25 22:17:29 -0300

    ABM Turno Terminado

--- UberFrba/GD1C2017DataSet1.Designer.cs
+++ UberFrba/GD1C2017DataSet1.Designer.cs
@@ -13037,7 +13037,7 @@ SELECT Turno_Descripcion, Turno_Horario_Inicio, Turno_Horario_Fin, Turno_Precio_
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[4];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[5];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT Turno_Descripcion, Turno_Horario_Inicio, Turno_Horario_Fin, Turno_Precio_B" +
@@ -13065,6 +13065,19 @@ SELECT Turno_Descripcion, Turno_Horario_Inicio, Turno_Horario_Fin, Turno_Precio_
             this._commandCollection[3].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Horario_Fin", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 0, "Turno_Horario_Fin", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
             this._commandCollection[3].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Precio_Base", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 2, "Turno_Precio_Base", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
             this._commandCollection[3].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Valor_Kilometro", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 2, "Turno_Valor_Kilometro", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[4].Connection = this.Connection;
+            this._commandCollection[4].CommandText = @"UPDATE       OVERFANTASY.Turno
+SET                Turno_Horario_Inicio = @Turno_Horario_Inicio, Turno_Horario_Fin = @Turno_Horario_Fin, Turno_Precio_Base = @Turno_Precio_Base, Turno_Valor_Kilometro = @Turno_Valor_Kilometro, 
+                         Turno_Estado = @Turno_Estado
+WHERE        (Turno_Descripcion = @Original_Turno_Descripcion); ";
+            this._commandCollection[4].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Horario_Inicio", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 0, "Turno_Horario_Inicio", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Horario_Fin", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 0, "Turno_Horario_Fin", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Precio_Base", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 2, "Turno_Precio_Base", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Valor_Kilometro", global::System.Data.SqlDbType.Decimal, 9, global::System.Data.ParameterDirection.Input, 18, 2, "Turno_Valor_Kilometro", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Turno_Estado", global::System.Data.SqlDbType.Char, 1, global::System.Data.ParameterDirection.Input, 0, 0, "Turno_Estado", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[4].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Original_Turno_Descripcion", global::System.Data.SqlDbType.VarChar, 255, global::System.Data.ParameterDirection.Input, 0, 0, "Turno_Descripcion", global::System.Data.DataRowVersion.Original, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -13438,6 +13451,65 @@ SELECT Turno_Descripcion, Turno_Horario_Inicio, Turno_Horario_Fin, Turno_Precio_
             }
             else {
                 command.Parameters[4].Value = global::System.DBNull.Value;
+            }
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, false)]
+        public virtual int UpdateTurno(global::System.Nullable<decimal> Turno_Horario_Inicio, global::System.Nullable<decimal> Turno_Horario_Fin, global::System.Nullable<decimal> Turno_Precio_Base, global::System.Nullable<decimal> Turno_Valor_Kilometro, string Turno_Estado, string Original_Turno_Descripcion) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[4];
+            if ((Turno_Horario_Inicio.HasValue == true)) {
+                command.Parameters[0].Value = ((decimal)(Turno_Horario_Inicio.Value));
+            }
+            else {
+                command.Parameters[0].Value = global::System.DBNull.Value;
+            }
+            if ((Turno_Horario_Fin.HasValue == true)) {
+                command.Parameters[1].Value = ((decimal)(Turno_Horario_Fin.Value));
+            }
+            else {
+                command.Parameters[1].Value = global::System.DBNull.Value;
+            }
+            if ((Turno_Precio_Base.HasValue == true)) {
+                command.Parameters[2].Value = ((decimal)(Turno_Precio_Base.Value));
+            }
+            else {
+                command.Parameters[2].Value = global::System.DBNull.Value;
+            }
+            if ((Turno_Valor_Kilometro.HasValue == true)) {
+                command.Parameters[3].Value = ((decimal)(Turno_Valor_Kilometro.Value));
+            }
+            else {
+                command.Parameters[3].Value = global::System.DBNull.Value;
+            }
+            if ((Turno_Estado == null)) {
+                command.Parameters[4].Value = global::System.DBNull.Value;
+            }
+            else {
+                command.Parameters[4].Value = ((string)(Turno_Estado));
+            }
+            if ((Original_Turno_Descripcion == null)) {
+                throw new global::System.ArgumentNullException("Original_Turno_Descripcion");
+            }
+            else {
+                command.Parameters[5].Value = ((string)(Original_Turno_Descripcion));
             }
             global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
             if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
