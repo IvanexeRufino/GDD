01a1dc965891400c6227a92c3053ac019892271a
NicolasJC <nicolasjcnirius@gmail.com>
2017-05-29 17:53:46 -0300

    ABM Automovil sin terminar

--- UberFrba/Abm Automovil/ABMAutomovil.Designer.cs
+++ UberFrba/Abm Automovil/ABMAutomovil.Designer.cs
@@ -28,34 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gD1C2017DataSet = new UberFrba.GD1C2017DataSet();
+            this.automovilTableAdapter = new UberFrba.GD1C2017DataSetTableAdapters.AutomovilTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.automovilPatenteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.automovilMarcaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.automovilModeloDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.automovilEstadoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.turnoDescripcionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.choferUsernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modificar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Inhabilitar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gD1C2017DataSet)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataMember = "Automovil";
+            this.bindingSource1.DataSource = this.gD1C2017DataSet;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1017, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.automovilPatenteDataGridViewTextBoxColumn,
+            this.automovilMarcaDataGridViewTextBoxColumn,
+            this.automovilModeloDataGridViewTextBoxColumn,
+            this.automovilEstadoDataGridViewTextBoxColumn,
+            this.turnoDescripcionDataGridViewTextBoxColumn,
+            this.choferUsernameDataGridViewTextBoxColumn,
+            this.Modificar,
+            this.Inhabilitar});
+            this.dataGridView1.DataSource = this.bindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 207);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(853, 262);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // gD1C2017DataSet
+            // 
+            this.gD1C2017DataSet.DataSetName = "GD1C2017DataSet";
+            this.gD1C2017DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // automovilTableAdapter
+            // 
+            this.automovilTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(856, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Filtrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button2.Location = new System.Drawing.Point(856, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button3.Location = new System.Drawing.Point(697, 505);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Alta";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button4.Location = new System.Drawing.Point(52, 505);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 45);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Cancelar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // automovilPatenteDataGridViewTextBoxColumn
+            // 
+            this.automovilPatenteDataGridViewTextBoxColumn.DataPropertyName = "Automovil_Patente";
+            this.automovilPatenteDataGridViewTextBoxColumn.HeaderText = "Automovil_Patente";
+            this.automovilPatenteDataGridViewTextBoxColumn.Name = "automovilPatenteDataGridViewTextBoxColumn";
+            // 
+            // automovilMarcaDataGridViewTextBoxColumn
+            // 
+            this.automovilMarcaDataGridViewTextBoxColumn.DataPropertyName = "Automovil_Marca";
+            this.automovilMarcaDataGridViewTextBoxColumn.HeaderText = "Automovil_Marca";
+            this.automovilMarcaDataGridViewTextBoxColumn.Name = "automovilMarcaDataGridViewTextBoxColumn";
+            // 
+            // automovilModeloDataGridViewTextBoxColumn
+            // 
+            this.automovilModeloDataGridViewTextBoxColumn.DataPropertyName = "Automovil_Modelo";
+            this.automovilModeloDataGridViewTextBoxColumn.HeaderText = "Automovil_Modelo";
+            this.automovilModeloDataGridViewTextBoxColumn.Name = "automovilModeloDataGridViewTextBoxColumn";
+            // 
+            // automovilEstadoDataGridViewTextBoxColumn
+            // 
+            this.automovilEstadoDataGridViewTextBoxColumn.DataPropertyName = "Automovil_Estado";
+            this.automovilEstadoDataGridViewTextBoxColumn.HeaderText = "Automovil_Estado";
+            this.automovilEstadoDataGridViewTextBoxColumn.Name = "automovilEstadoDataGridViewTextBoxColumn";
+            // 
+            // turnoDescripcionDataGridViewTextBoxColumn
+            // 
+            this.turnoDescripcionDataGridViewTextBoxColumn.DataPropertyName = "Turno_Descripcion";
+            this.turnoDescripcionDataGridViewTextBoxColumn.HeaderText = "Turno_Descripcion";
+            this.turnoDescripcionDataGridViewTextBoxColumn.Name = "turnoDescripcionDataGridViewTextBoxColumn";
+            // 
+            // choferUsernameDataGridViewTextBoxColumn
+            // 
+            this.choferUsernameDataGridViewTextBoxColumn.DataPropertyName = "Chofer_Username";
+            this.choferUsernameDataGridViewTextBoxColumn.HeaderText = "Chofer_Username";
+            this.choferUsernameDataGridViewTextBoxColumn.Name = "choferUsernameDataGridViewTextBoxColumn";
+            // 
+            // Modificar
+            // 
+            this.Modificar.HeaderText = "Modificar";
+            this.Modificar.Name = "Modificar";
+            // 
+            // Inhabilitar
+            // 
+            this.Inhabilitar.HeaderText = "Inhabilitar";
+            this.Inhabilitar.Name = "Inhabilitar";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 26);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(6, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.Size = new System.Drawing.Size(57, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Primer ABM";
+            this.label1.Text = "Patente";
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ABMAutomovil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1064, 572);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ABMAutomovil";
+            this.Text = "ABMAutomovil";
+            this.Load += new System.EventHandler(this.ABMAutomovil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gD1C2017DataSet)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private GD1C2017DataSet gD1C2017DataSet;
+        private GD1C2017DataSetTableAdapters.AutomovilTableAdapter automovilTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn automovilPatenteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn automovilMarcaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn automovilModeloDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn automovilEstadoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn turnoDescripcionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn choferUsernameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Modificar;
+        private System.Windows.Forms.DataGridViewButtonColumn Inhabilitar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+
 
     }
 }
--- UberFrba/Abm Automovil/ABMAutomovil.cs
+++ UberFrba/Abm Automovil/ABMAutomovil.cs
@@ -16,5 +16,59 @@ namespace UberFrba.Abm_Automovil
         {
             InitializeComponent();
         }
+
+        private void ABMAutomovil_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'gD1C2017DataSet.Automovil' table. You can move, or remove it, as needed.
+            this.automovilTableAdapter.Fill(this.gD1C2017DataSet.Automovil);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Agregar_Modificar_Automovil ama = new Agregar_Modificar_Automovil();
+            //ama.Show();
+        }
+
+        //fillby falta declarar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //automovilTableAdapter.FillBy(gD1C2017DataSet.Turno, textBox1.Text);
+        }
+
+        //no se q es pero lo puse por las dudas y no exploto
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            automovilTableAdapter.Fill(gD1C2017DataSet.Automovil);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 6)
+            {
+                //Agregar_Modificar_Automovil ama = new Agregar_Modificar_Automovil(dataGridView1.Rows[e.RowIndex]);
+                //ama.Show();
+            }
+            else
+            {
+                if (e.ColumnIndex == 7)
+                {
+                    //automovilTableAdapter.DeleteAutomovil(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                    //MessageBox.Show("El vehiculo se ha Inhabilitado Correctamente", "Baja Turno", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    //automovilTableAdapter.Fill(gD1C2017DataSet.Automovil);
+                }
+            }
+        }
     }
 }
