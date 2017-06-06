3b23ae951ef24cefd71000761c8de6be4d0c86f2
ivanexerufino <ivan-2100@hotmail.com>
2017-06-06 14:54:56 -0300

    Buscador simultaneo en chofer

--- UberFrba/Abm Chofer/ABMChofer.cs
+++ UberFrba/Abm Chofer/ABMChofer.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace UberFrba.Abm_Chofer
 {
     public partial class ABMChofer : Form
     {
+        BaseDeDatos bd;
+        SqlConnection conexion;
+
         public ABMChofer()
         {
+            bd = new BaseDeDatos();
+            conexion = bd.getCon();
             InitializeComponent();
+        }
+
+        private void ABMChofer_Load(object sender, EventArgs e)
+        {
+            DataGridViewButtonColumn modificarChofer = new DataGridViewButtonColumn();
+            modificarChofer.Name = "Modificar Chofer";
+            modificarChofer.Text = "Modificar Chofer";
+
+            DataGridViewButtonColumn inhabilitarCliente = new DataGridViewButtonColumn();
+            inhabilitarCliente.Name = "Inhabilitar Chofer";
+            inhabilitarCliente.Text = "Inhabilitar Chofer";
+
+            String select = "SELECT Usuario_Username, Chofer_Nombre, Chofer_Apellido, Chofer_DNI, Chofer_FechaNacimiento, Chofer_Direccion, Chofer_Piso, Chofer_Departamento, Chofer_CodigoPostal, Chofer_Mail, Chofer_telefono, Chofer_Localidad, Usuario_Estado FROM OVERFANTASY.ChoferCompleto";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(select, conexion);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+            if (dataGridView1.Columns["Modificar Chofer"] == null)
+            {
+                dataGridView1.Columns.Insert(13, modificarChofer);
+            }
+            if (dataGridView1.Columns["Inhabilitar Chofer"] == null)
+            {
+                dataGridView1.Columns.Insert(14, inhabilitarCliente);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String filtro = "";
+            String and = " AND ";
+            filtro += "Chofer_Nombre LIKE '%" + textBox1.Text + "%'";
+            filtro += and;
+            filtro += "Chofer_Apellido LIKE '%" + textBox2.Text + "%'";
+            if (!textBox3.Text.Equals(""))
+            {
+                filtro += and;
+                filtro += "Chofer_DNI = '" + textBox3.Text + "'";
+            }
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = filtro;
+
         }
     }
 }
