a7696219a97961e7827a202bb5f02bc73a35d1db
Mathisoad <mathisoad2@gmail.com>
2017-06-11 20:34:13 -0300

    Comentarios codigo cliente

--- UberFrba/Abm Cliente/ABMCliente.designer.cs
+++ UberFrba/Abm Cliente/ABMCliente.designer.cs
@@ -153,6 +153,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(138, 31);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button6
             // 
